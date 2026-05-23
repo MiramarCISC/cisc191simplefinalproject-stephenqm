5d226754173fbd21553213a32a69db16399caaf9
stephenqm <smckinney006@student.sdccd.edu>
2026-05-23 11:53:57 -0700

    Changed WebInterfaceTest.java

--- src/main/java/edu/sdccd/cisc191/game/exception/InvalidMatchException.java
+++ src/main/java/edu/sdccd/cisc191/game/exception/InvalidMatchException.java
@@ -1,4 +1,17 @@
 package edu.sdccd.cisc191.game.exception;
 
-public class InvalidMatchException {
+
+// MODULE 4: Exceptions, File I/O, Database Persistence
+// Custom unchecked exception thrown when match input is invalid
+public class InvalidMatchException extends RuntimeException {
+
+    // Basic constructor with just a message
+    public InvalidMatchException(String message) {
+        super(message);
+    }
+
+    // Constructor with message + root cause (for wrapping other exceptions)
+    public InvalidMatchException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
